4852702c806ae9e2c816e1013739b73b69441cf5
nehashah4590 <ns457989@gmail.com>
2024-04-12 18:56:31 +0545

    first commit

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,40 @@
+"use client"
+import React, {useState} from 'react';
+import Link from 'next/link';
+import menu from '../../public/menu.png';
+import Image from 'next/image';
+
+interface NavbarProps {
+  contact: string;
+  project: string;
+
+}
+
+const Navbar: React.FC<NavbarProps> = ({ contact, project})=>{
+  const [isOpen, setIsOpen]= useState(false);
+
+  return (
+    <div className='h-[80px] p-2 flex flex-col md:flex md:flex-row font-semibold lg:mx-40'>
+      <div className=' h-full w-full  p-2 flex justify-between items-center'>
+        <h1 className=' px-2 tracking-[1px] hover:text-gray-500'><Link href="./">NEHA SHAH</Link></h1>
+        <button
+          type='button'
+          className='md:hidden '
+          onClick={()=>{setIsOpen(!isOpen)}}> 
+          <Image src={menu} alt="menu button"/>
+        </button>
+      </div>
+     
+        <div className='h-full p-2 md:mx-8 md:px-8 flex  items-center w-full justify-start md:justify-end'>
+        <ul className={` lg:mr-6  gap-10  ${isOpen? "sm:absolute sm:top-12 bg-white shadow-md md:shadow-none w-full p-2 mt-2 md:mt-0 md:p-0 z-20 md:static md:flex md:justify-end md:items-center":"hidden md:flex justify-center items-center"}`}>
+          <Link href="./"><li className='hover:text-gray-400'>About</li></Link>
+          <Link href={`#${project}`} ><li className='hover:text-gray-500'>Projects</li></Link>
+          <Link href={`#${contact}`} ><li className='hover:text-gray-500'>Contact</li></Link>
+        </ul>
+      </div>
+ 
+    </div>
+  )
+}
+
+export default Navbar
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,73 @@
 import Image from "next/image";
+import pp from "../public/pp.png";
+import GPTNepal from "../public/GPTNepal.png";
+import shivtrading from "../public/shivtrading.png";
+import heart from "../public/heart.png";
+import Link from "next/link";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+   <>
+   <Navbar contact="contact" project="project"/>
+   <main>
+     <div className="flex flex-col md:flex md:flex-row lg:mx-40 my-4">
+      <div className="w-auto md:w-[70%] px-7 md:p-8">
+      <Image src={pp} alt="pp" className="md:hidden m-4 ml-16 shadow-lg shadow-gray-500 rounded-full object-cover h-[200px] w-[200px]"/>
+      <h1 className="text-3xl font-bold pb-9">Hello! I'm Neha, a developer from Nepal.</h1>
+     <p className="pb-2 text-start">I love building tools that are user-friendly, simple and delightful.</p>
+     <p className="pb-2 text-start">I was a student at Purwanchal Campus, Dharan 
+      where I completed my four years Computer Engineering Course. My active participation
+       in different software development workshops and competitions during my college time 
+       started my interest in Frontend Development.</p>
+       <p className="pb-2 text-start">
+        Through these experiences, I had the opportunity to work as a Intern in SEDS Nepal where my job 
+        was to collaborate with other teams members to make database 
+        management Software. I also got the opportunity to work as a freelancer in different website  projects.
+       </p>
+       <p className="pb-2">I'm currently looking for a new role as a Frontend Developer. Hire me?</p>
+       <div className="flex">
+        <button className="bg-black text-white text-sm hover:bg-white hover:text-black border rounded-sm px-8 py-2 m-4"> <Link href="https://www.linkedin.com/in/neha-shah-a528801b3/">View Linkedin</Link> </button>
+        <button className=" border rounded-sm px-8 text-sm py-2 m-4 hover:bg-black hover:text-white"><Link href="https://github.com/nehashah4590">View Github</Link> </button>
+       </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="hidden md:flex justify-center py-8 pb-12 md:pr-2 lg:pr-0">
+          <Image src={pp} alt="pp" className=" shadow-lg shadow-gray-500 rounded-lg object-cover"/>
       </div>
+     </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+     {/* projects */}
+     <div className="lg:mx-40 my-20 px-8" id="project">
+      <h1 className="pb-2 text-4xl font-bold">Projects</h1>
+      <p className="pb-8">Some of the side projects, I have currently worked on:</p>
+      <div className="flex flex-col md:flex md:flex-row gap-10">
+      <div className="lg:h-[60vh] lg:w-[35vw] p-2">
+        <Image src={GPTNepal} alt="project" className=" shadow-lg shadow-gray-500 rounded-lg object-cover"/>
+        <h2 className="p-2 font-bold text-2xl mt-4">GPT Nepal</h2> 
+        <p className="px-2 pb-2">A web based QA App based on transformer model.</p>
+        <Link href="https://vercel.com/nehashah4590s-projects/qa-app"><p className="font-semibold text-md px-2">View Project</p></Link>
       </div>
-    </main>
+      <div className=" lg:h-[60vh] lg:w-[35vw] p-2">
+        <Image src={shivtrading} alt="project" className=" shadow-lg shadow-gray-500 rounded-lg object-cover"/>
+        <h2 className="p-2 font-bold text-2xl mt-4">Shiv trading</h2>  
+        <p className="px-2 pb-2">It is a tiles and marble displaying website. </p>
+        <Link href="https://shiv-trading.com"><p className="font-semibold text-md px-2">View Project</p></Link>       
+      </div>
+      </div>
+     </div>
+
+        {/* contact */}
+        <div className="lg:mx-40 my-20 m-2 " id="contact">
+      <h1 className="pb-2 text-4xl font-bold">Get in touch</h1>
+      <p className="pb-28">Do you have a job opportunity or idea you'd like to discuss? Feel free to reach me at<span className="bg-gradient-to-r from-red-500 to-yellow-500 text-transparent bg-clip-text font-semibold"> er.nehashah822@gmail.com.</span> You
+      can also find me on <Link href="https://twitter.com/Ne_ha_Sh_ah" className="bg-gradient-to-r from-red-500 to-yellow-500 text-transparent bg-clip-text font-semibold">Twitter</Link>, 
+       <Link href="https://github.com/nehashah4590" className="bg-gradient-to-r from-red-500 to-yellow-500 text-transparent bg-clip-text font-semibold"> Github</Link> and 
+       <Link href="https://www.linkedin.com/in/neha-shah-a528801b3/" className="bg-gradient-to-r from-red-500 to-yellow-500 text-transparent bg-clip-text font-semibold"> Linkedin.</Link></p>
+      <hr/>
+      <p className="py-8 flex pb-32">Built with TypeScript, Tailwind and Next.js <Image src={heart} alt="heart" className="h-[22px] w-[22px] mt-[1px] mx-1"/></p>
+      </div>
+     
+   </main>
+   </>
   );
 }
