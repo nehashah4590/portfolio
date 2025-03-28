c8aa65dcc7029108577b8196b07c8d256bd49c1a
nehashah4590 <ns457989@gmail.com>
2025-03-29 00:35:34 +0545

    fix

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -59,7 +59,7 @@ export default function Navbar() {
           ))}
           <ModeToggle />
           <Button size="sm">
-            <a href="/neha_resume.pdf" target="_blank" rel="noopener noreferrer">
+            <a href="/neha_resume.pdf" target="_blank" >
                 Resume
               </a>
             </Button>
@@ -91,7 +91,7 @@ export default function Navbar() {
               </Link>
             ))}
             <Button size="sm" className="w-full">
-              <a href="/neha_resume.pdf" target="_blank" rel="noopener noreferrer">
+              <a href="/neha_resume.pdf" target="_blank" >
                 Resume
               </a>
             </Button>
